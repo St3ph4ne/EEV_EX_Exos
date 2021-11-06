e443a7e4117372e76ba82b599177e3aa392cdcac
Steph <spodar.s@protonmail.com>
2021-11-06 11:47:50 +0000

    flag et incrementation

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
-let empty = true;
-let fully = true;
+let flag = true;
+let counter = 0;
 
+
+// si flag vaut false incrémente la variable counter
+if(!flag) counter++;
+
+
+// si flag vaut false décrémente la variable counter
+if(flag) counter--;
+
+
+// on inverse la valeur de la variable booléenne flag
+flag = !flag;
+// maintenant flag vaut donc false
+
+// si flag vaut true incrémente la variable counter
+if(flag) counter++;
+
+
+// si flag vaut false décrémente la variable counter
+if(!flag) counter--;
+
+// initialement flag vaut true, donc
+// la condition ligne 6 n'est pas validée
+// la condition ligne 10 est validée, --> counter=-1
+// ligne 14 on inverse la valeur de flag --> flag=false
+// la condition ligne 18 n'est pas validée
+// la condition ligne 22 est validée --> counter=-1-1=-2
--- readme.md
+++
@@ -1,4 +0,0 @@
-On déclare les variables booléennes empty et true,
-en leur attribuant une des 2 valeurs possibles: true ou false.
-
-L'interpréteur saura automatiquement qu'il s'agit de variables booléennes.
